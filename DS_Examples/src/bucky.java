d9c145d5f541ec9cbdb36e9fd8ba59be96915116
EmilioJagshamesh <elmijito1990@gmail.com>
2021-03-30 21:36:59 -0600

    first commit

---
+++ DS_Examples/src/bucky.java
@@ -0,0 +1,20 @@
+
+public class bucky {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] words = {"funk", "furry","chunk", "bruk", "truck","muck","dandeelion"};
+		
+		for(String str : words)
+		{
+			if(str.startsWith("fu"))
+				System.out.println(str + " - starts with 'fu'");
+		}
+		
+		
+		
+		
+	}
+
+}
